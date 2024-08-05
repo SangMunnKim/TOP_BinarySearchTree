bd69934e190e671254afe97c53904f98abaca917
Sang <108661792+SangMunnKim@users.noreply.github.com>
2024-08-05 13:08:08 +0200

    add deleteItem, find, levelOrder, inOrder

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -30,6 +30,7 @@ class Tree {
 
     insert(value, root = this.root) {
 
+        //base case
         if (root === null) {
           root = new Node(value);
           return root;
@@ -44,46 +45,108 @@ class Tree {
         return root;
     }
 
-    deleteItem(value) {
+    deleteItem(value, root = this.root) {
+        //base case
+        if (root === null) {
+            return root;
+        }
+
+        //traverse to the left if value smaller than root
+        if (value < root.data) {
+            root.left = this.deleteItem(value, root.left);
+        } 
+        //traverse to the right if value bigger than root
+        else if (value > root.data ) {
+            root.right = this.deleteItem(value, root.right);
+        }
+        //delete node if value matches
+        else {
+            //case 1: the node has one or no child nodes
+            if (root.left === null) {
+                return root.right;
+            }
+            else if (root.right === null) {
+                return root.left;
+            }
+            //case 2: the node has two child nodes
+            else {
+                const minData = function nextSmallestRightData(root) {
+                    let min = root.data;
+                    let newRoot = root;
+
+                    while(newRoot.left !== null) {
+                        newRoot = newRoot.left;
+                        min = newRoot.data;
+                    }
+
+                    return min;
+                }
+
+                root.data = minData(root.right);
+
+                root.right = this.deleteItem(root.data, root.right);
+            }
+        }
+        return root;
+    }
+
+    find(value, root = this.root) {
+        if (root.data === null || root.data === value) return root;
+
+        if (value < root.data) {
+            return this.find(root.data, root.left);
+        } 
+        else if (value > root.data) {
+            return this.find(root.data, root.right);
+        }
+    }
+
+    levelOrder(callback) {
+        if (typeof callback !== 'function') {
+            throw new Error('Callback needs to be a function!');
+        }
+
+        const queue = [this.root];
+
+        while (queue.length > 0) {
+            const currentNode = queue.shift();
+            callback(currentNode);
+            
+            if (currentNode.left !== null) {
+                queue.push(currentNode.left);
+            }
+            
+            if (currentNode.right !== null) {
+                queue.push(currentNode.right);
+            }
+        }
+
+    }
+
+    inOrder(callback, root = this.root) {
+        if (typeof callback !== 'function') {
+            throw new Error('Callback needs to be a function!');
+        }
+
+        if(root === null) {
+            return;
+        }
+
+        this.inOrder(callback, root.left);
+        callback(root);
+        this.inOrder(callback, root.right);
 
     }
 
     sort(array) {
-
         return array.sort((a, b) => a - b);
     }
 
     removeDuplicates(array) {
         return [...new Set(array)];
     }
+
+
 }
 
-// This function will expect to receive the root of your tree as the value for the node parameter.
-const prettyPrint = (node, prefix = "", isLeft = true) => {
-    if (node === null) {
-        return;
-    }
-    if (node.right !== null) {
-        prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
-    }
-    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
-    if (node.left !== null) {
-        prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
-    }
-};
-
-
-
-
-
-const exampleArray = [1, 9, 9, 8, 5, 4, 2, 7, 4, 3, 6, 10];
-const tree = new Tree(exampleArray);
-
-prettyPrint(tree.root);
-console.log('-----------------------------');
-
-tree.insert(11);
-tree.insert(13);
-tree.insert(0);
-
-prettyPrint(tree.root);
+export { Node, Tree }
---
+++ main.js
@@ -0,0 +1,38 @@
+import { Node, Tree } from './binary-search-tree.js';
+
+// This function will expect to receive the root of your tree as the value for the node parameter.
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+    if (node === null) {
+        return;
+    }
+    if (node.right !== null) {
+        prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+        prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+};
+
+const exampleArray = [1, 9, 9, 8, 5, 4, 2, 7, 4, 3, 6, 10];
+const tree = new Tree(exampleArray);
+
+prettyPrint(tree.root);
+console.log('-----------------------------');
+
+// tree.insert(11);
+// tree.insert(13);
+// tree.insert(0);
+
+// prettyPrint(tree.root);
+// tree.deleteItem(11);
+// prettyPrint(tree.root);
+// tree.deleteItem(13);
+// prettyPrint(tree.root);
+// tree.deleteItem(8);
+// prettyPrint(tree.root);
+
+// tree.levelOrder(node => console.log(node.data));
+
+tree.inOrder(node => console.log(node.data));
+
